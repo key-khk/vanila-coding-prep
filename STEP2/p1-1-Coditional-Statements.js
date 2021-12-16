91a43f6146442348d42253eb221961b5466abd82
key-khk <mkmc020@gmail.com>
2021-12-17 01:46:31 +0900

    2021.12.16

---
+++ STEP2/p1-1-Coditional-Statements.js
@@ -0,0 +1,27 @@
+// #1. 무엇이 출력될까요?  답: 1
+if (true) {
+    console.log(1);
+  } else if (true) {
+    console.log(2);
+  } else {
+    console.log(3);
+  }
+  
+  // #2. 무엇이 출력될까요? 답:2
+  if ("") {
+    console.log(1);
+  } else if ("a") {
+    console.log(2);
+  } else {
+    console.log(3);
+  }
+  
+  // #3. 무엇이 출력될까요? 답:2
+  if (null) {
+    console.log(1);
+  } else if (5) {
+    console.log(2);
+  } else {
+    console.log(3);
+  }
+  
---
+++ STEP2/p1-3-Exercise-2.js
@@ -0,0 +1,10 @@
+//#2.Biggest Number
+var a=10;
+var b=20;
+var c=30;
+if(b < a && c < a) {
+   console.log(a);
+} else if (a < b && c < b) {
+      console.log(b);
+} else {
+    console.log(c); }
